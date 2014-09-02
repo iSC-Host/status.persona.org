4a5ae3bc1588caad4926191aef2acb6d18e4b192
Travis Build <services-ops@mozilla.com>
2014-09-02 16:59:29 -0700

    Github Pages Build

--- scripts/run.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env node
-
-const express = require('express'),
-         path = require('path');
-
-var app = express()
-  .use(express.static(path.join(__dirname, "..", "html")))
-  .listen(process.env.PORT || 8080);
